62f3d663511625fb845baaea6f9fe75ddb200ad7
Ardavan Zareirad <ardavan2525@gmail.com>
2023-04-15 17:39:39 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(){
+   let returnFirstTwoDrivers = (['Antonia', 'Nuru'])
+return returnFirstTwoDrivers;
+}
+const returnLastTwoDrivers = function() {
+    let returnLastTwoDrivers = (['Amari', 'Mo'])
+    return returnLastTwoDrivers;
+}
+const selectingDrivers = ([returnFirstTwoDrivers, returnLastTwoDrivers]);
+
+function createFareMultiplier(integer) {
+    return function (fare) {
+    return integer * fare;
+    };
+}
+const fareDoubler = function (createFareMultiplier) {
+    return createFareMultiplier * 2;
+}
+const fareTripler = function (createFareMultiplier) {
+    return createFareMultiplier * 3;
+}
+let Scuber = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+function selectDifferentDrivers(Scuber,returnFirstTwoDrivers) {
+   return returnFirstTwoDrivers(Scuber);
+}
+
+
+
+
+
+
+
+
+
